dfbaf52c2b7a434a7f69d29dae83d055e50c3060
anush-2007 <anushbhardwaj2007@gmail.com>
2024-08-06 21:20:48 +0530

    react commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import logo from "../assets/logo.svg";
 import { BiChevronUp, BiChevronDown } from "react-icons/bi";
 import { motion } from "framer-motion";
